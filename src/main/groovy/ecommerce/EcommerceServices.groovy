05393ac4310e1223abd4f923e5788ea9167489bd
Giulio Speri <giulio.speri@mpstyle.it>
2026-03-18 15:14:28 +0100

    [F2E-23 | BE: servizio + script per filtri listing - Filtro AND/OR + prio filtro caratteristiche]

--- src/main/groovy/ecommerce/EcommerceServices.groovy
+++ src/main/groovy/ecommerce/EcommerceServices.groovy
@@ -3,6 +3,7 @@ package ecommerce
 import org.apache.ofbiz.entity.GenericValue
 import org.apache.ofbiz.entity.condition.EntityCondition
 import org.apache.ofbiz.entity.condition.EntityOperator
+import org.apache.ofbiz.entity.util.EntityUtilProperties
 
 def performCategoryFilterSearch() {
 
@@ -12,11 +13,38 @@ def performCategoryFilterSearch() {
     String prodCatalogId = parameters.prodCatalogId
     boolean limitView = parameters.limitView ?: false
     int defaultViewSize = parameters.defaultViewSize
+    boolean filterOutOfStockVariants = parameters.filterOutOfStockVariants ?: false
+    String MPSTYLE_SYSTEM_RESOURCE_ID = "mpstyle"
+
     def returnMessage = ""
     /*<K,V> : <featureTypeId, List<featureValueId>>: creation with HashSet to have unique values
      * but then converted to List before returning, to be easier to handle on the UI side
      */
     Map appliedFiltersByFeatureType = [:].withDefault { new HashSet<String>() }
+
+    /* Read some configuration parameters to drive the filtering priorities among featureTypes and the logical
+       operator to apply (OR vs AND) when multiple feature values are selected.
+       Default operator: OR;
+       Filtering priority: only if operator is AND
+     */
+    String filterOperator = EntityUtilProperties.getPropertyValue(MPSTYLE_SYSTEM_RESOURCE_ID, "catalog.filterOperator", "OR", delegator)
+    String featureTypesFilterPrio = EntityUtilProperties.getPropertyValue(MPSTYLE_SYSTEM_RESOURCE_ID, "catalog.featureTypesFilterPrio", "", delegator)
+
+    String []featureTypesFilterPrioArr = featureTypesFilterPrio ? featureTypesFilterPrio.split(",") : []
+    List<String> featureTypesFilterPrioList = featureTypesFilterPrioArr ? featureTypesFilterPrioArr.toList() : []
+
+    /* If operator is AND and is not defined a priority among feature types for filtering build
+     * the list with the keys (featureTypeId) of the filterFeatureMap, to apply the filtering on all the feature types in the filter, without any priority
+     */
+    if (filterOperator.equalsIgnoreCase("AND") && featureTypesFilterPrioList.isEmpty()) {
+        featureTypesFilterPrioList = filterFeatureMap.keySet().toList()
+
+        /* TODO check on featureTypes existence
+        def featureTypes = from('ProductFeatureType').where(EntityCondition.makeCondition('productFeatureTypeId', EntityOperator.IN, featureTypesFilterPrioList)).cache(true).queryList()
+        */
+    }
+
+    logInfo("performCategoryFilterSearch: filterOperator = ${filterOperator}, featureTypesFilterPrioList = ${featureTypesFilterPrioList}")
 
     //Get product category members with the same logic as categorydetail screen, then apply filters on them
     def catAndMemberMap = [productCategoryId: productCategoryId,
@@ -33,46 +61,73 @@ def performCategoryFilterSearch() {
     productCategory = catAndMemberResult.productCategory
     productCategoryMembers = catAndMemberResult.productCategoryMembers
 
+    //Get all the features application for the products in the category, to have them in cache for the filtering logic later
+    List<String> productIdList = productCategoryMembers.collect() { it.productId }
+    List<GenericValue> productFeaturesAndAppl = from("ProductFeatureAndAppl")
+                                                .where(EntityCondition.makeCondition('productId', EntityOperator.IN, productIdList))
+                                                .cache(true)
+                                                .filterByDate()
+                                                .queryList()
+
+    //Build an index of the features application by featureTypeId and featureId,
+    // to be able to easily retrieve the products matching the filter criteria in the next steps
+    Map featureApplIndex = [:].withDefault { [:].withDefault { new HashSet<>() } }
+    productFeaturesAndAppl.each { gv ->
+        featureApplIndex[gv.productFeatureTypeId][gv.productFeatureId].add(gv.productId)
+    }
+    //Description to be displayed in the UI as applied filter value
+    Map featureValueDescriptionMap = productFeaturesAndAppl.collectEntries { gv -> [(gv.productFeatureId) :  (gv.description ?: gv.productFeatureId)] }
+
     List<GenericValue> filteredProductCategoryMembers = []
     if (productCategoryMembers) {
-        for (productCategoryMember in productCategoryMembers) {
-            productId = productCategoryMember.productId
-            def featureTypeMatchCount = 0
-            for (featureTypeId in filterFeatureMap.keySet()) {
+
+        Set<String> baseCategoryMemberProductIdSet = productCategoryMembers.collect { it.productId }.toSet()
+        Set<String> filteredPcmSet = null
+
+        if (filterOperator.equalsIgnoreCase("AND")) {
+            logInfo("performCategoryFilterSearch2: applying AND logic among filter values, with filtering priority among feature types: ${featureTypesFilterPrioList}")
+            filteredPcmSet = new HashSet<>(baseCategoryMemberProductIdSet)
+            featureTypesFilterPrioList.each { featureTypeId ->
                 List featureValueIdList = filterFeatureMap.get(featureTypeId)
                 if (featureValueIdList) {
-                    EntityCondition featureTypeCond = EntityCondition.makeCondition(EntityOperator.AND,
-                            EntityCondition.makeCondition('productId', EntityOperator.EQUALS, productId),
-                            EntityCondition.makeCondition('productFeatureTypeId', EntityOperator.EQUALS, featureTypeId),
-                            EntityCondition.makeCondition('productFeatureId', EntityOperator.IN, featureValueIdList))
-                    productFeatures = from('ProductFeatureAndAppl').where(featureTypeCond).cache(true).filterByDate().queryList()
-                    if (productFeatures && !productFeatures.isEmpty()) {
-                        featureTypeMatchCount++
-                        List featureValueDescriptionList = productFeatures.collect { it.description ?: it.productFeatureId }
-                        /* This works along with the .withDefault { new HashSet<>() } I used to initialize the appliedFiltersByFeatureType map,
-                         * to keep track of the filters that actually matched for the products in the category,
-                         * and that I can then use to display only the relevant filters on the UI
-                         */
-                        appliedFiltersByFeatureType[featureTypeId].addAll(featureValueDescriptionList)
+                    Set<GenericValue> matchingProductsByFeatureType = new HashSet<>()
+                    featureValueIdList.each { productFeatureId ->
+                        matchingProductsByFeatureType.addAll(featureApplIndex[featureTypeId]?.get(productFeatureId) ?: Collections.emptySet())
+                        appliedFiltersByFeatureType[featureTypeId].add(featureValueDescriptionMap[productFeatureId])
+                    }
+                    filteredPcmSet.retainAll(matchingProductsByFeatureType)
+                }
+            }
+
+        } else if (filterOperator.equalsIgnoreCase("OR")) {
+            logInfo("performCategoryFilterSearch2: applying OR logic among filter values")
+            filteredPcmSet = new HashSet<>()
+            filterFeatureMap.each { featureTypeId, featureValueIdList ->
+                featureValueIdList.each { productFeatureId ->
+                    Set<GenericValue> matchingProductsByFeatureType = featureApplIndex[featureTypeId][productFeatureId] ?: Collections.emptySet()
+                    if (matchingProductsByFeatureType) {
+                        filteredPcmSet.addAll(matchingProductsByFeatureType)
+                        appliedFiltersByFeatureType[featureTypeId].add(featureValueDescriptionMap[productFeatureId])
                     }
                 }
             }
-            //If I have a match for at least one of the feature types in the filter, then I keep the product category member in the result
-            if (featureTypeMatchCount > 0) {
-                if (!filteredProductCategoryMembers) {
-                    filteredProductCategoryMembers = []
-                }
-                filteredProductCategoryMembers.add(productCategoryMember)
+        } else {
+            logWarning("performCategoryFilterSearch2: no valid filterOperator defined, skipping filtering and returning all the products in the category")
+            filteredPcmSet = baseCategoryMemberProductIdSet
+        }//end if filterOperator
+
+        if (filteredPcmSet && !filteredPcmSet.isEmpty()) {
+            filteredProductCategoryMembers = productCategoryMembers.findAll { gv ->
+                filteredPcmSet.contains(gv.getString("productId"))
             }
+            returnMessage = "Found ${filteredProductCategoryMembers.size()} product category members matching the filter criteria for category ${productCategoryId}."
+        } else {
+            returnMessage = "No products found matching the filter criteria for category ${productCategoryId}."
         }
-        // Convert the sets to lists before returning, to be easier to handle on the UI side
-        if (appliedFiltersByFeatureType && !appliedFiltersByFeatureType.isEmpty()) {
-            appliedFiltersByFeatureType = appliedFiltersByFeatureType.collectEntries { k, v -> [ (k) : new ArrayList(v) ] }
-        }
-        returnMessage = "Found ${filteredProductCategoryMembers.size()} product category members matching the filter criteria for category ${productCategoryId}."
+
     } else {
         filteredProductCategoryMembers = []
-        returnMessage = "No product category members found for category ${productCategoryId}. Nothing to filter."
+        returnMessage = "No product category members found for category ${productCategoryId}. Nothing to filter."//end if productCategoryMembers
     }
 
     Map returnMap = success(returnMessage)
